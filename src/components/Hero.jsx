4908cd6c838dd3d14acb499143e988464cb3d36f
Rahul-CSEStudent <talktorahul.b@gmail.com>
2023-05-28 15:57:27 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,10 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      <div className='hidden sm:contents'>
+        <ComputersCanvas />
+      </div>
+
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
